e7c872a40f7c3320b09a5762ad57b4a24b55cd77
miau23 <mia23@live.unc.edu>
2023-04-16 23:46:20 -0400

    finishing nav bar + help page

--- src/Help.js
+++ src/Help.js
@@ -1,10 +1,86 @@
 import React from 'react';
 import {NavBar} from './NavBar'
+import { Row, Col } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import './Help.css';
+
 
 export const Help = () => {
     return (
     <div>
         <NavBar></NavBar>
+        <Row>
+            <Col align="center">
+                <h1 className = "header-1"> FAQ</h1>
+                <h2 className = "header-2"> What is UrNextConnection?</h2>
+                <h3 className = "header-3"> UrNextConnection is a website that allows current and graduated students of UNC Chapel Hill to find other UNC people in their city! Connect with other Tar Heels to get recommendations of things to do in your city, find potential roommates and friends, help newcomers navigate the area, and more.</h3>
+                <h2 className = "header-2">How long does verification usually take?</h2>
+                <h3 className = "header-3">Verification is usually instantaneous when using a UNC email. If you are an alumni, we should usually verify you in a matter of minutes but this can sometimes take up to 2 days.</h3>
+                </Col>
+        </Row>
+        <Row>
+            <Col align="right">
+                <h2 className = "header-2" id="seemore"> see more</h2>
+            </Col>
+        </Row>
+        <Row>
+            <Col align="left" className ="contact-form">
+            <h1 className = "header-1">Contact Us</h1>
+                <form>                                           
+                    <div>
+                        <label className = "labels" htmlFor="Name">
+                            Name
+                        </label>
+                        <input
+                            id="name"
+                            name="name"
+                            type="name"                                    
+                            required                                                                                
+                        />
+                    </div>
+                    <div>
+                        <label className = "labels" htmlFor="Email">
+                            Email
+                        </label>
+                        <input
+                            id="email"
+                            name="email"
+                            type="email"                                    
+                            required                                                                                
+                        />
+                    </div>
+                    <div>
+                        <label className = "labels" htmlFor="Subject">
+                            Subject
+                        </label>
+                        <input
+                            id="subject"
+                            name="subject"
+                            type="subject"                                    
+                            required                                                                                
+                        />
+                    </div>
+                    <div>
+                        <label className = "labels" htmlFor="Message">
+                            Message
+                        </label>
+                        <input
+                            id="message"
+                            name="message"
+                            type="message"                                    
+                            required                                                                                
+                        />
+                    </div>
+                                        
+                    <div className ="sendbtnholder">
+                        <Button className="send-button">  
+                            Send!                                                                  
+                        </Button>
+                    </div>                               
+                </form>
+
+            </Col>
+        </Row>
     </div>
     )
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap'
 import logo from "./UrNextConnectionLogo.png";
+import './NavBar.css';
 
 export const NavBar = () => {
     return (
     <div>
-       <Navbar expand="lg">
+       <Navbar expand="lg" className = "nav-bar">
             <LinkContainer to="/homePage">
                 <Navbar.Brand>
                     <img src={logo} alt="UrNextConnection Logo"></img>  
@@ -16,16 +17,16 @@ export const NavBar = () => {
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <LinkContainer to="/homePage">
-                        <Nav.Link> Explore </Nav.Link>
+                        <Nav.Link className = "nav-titles"> Explore </Nav.Link>
                     </LinkContainer>
                     <LinkContainer to="/connections">
-                        <Nav.Link> Connections </Nav.Link>
+                        <Nav.Link className = "nav-titles"> Connections </Nav.Link>
                     </LinkContainer>
                     <LinkContainer to="/profile">
-                        <Nav.Link> Profile </Nav.Link>
+                        <Nav.Link className = "nav-titles"> Profile </Nav.Link>
                     </LinkContainer>
                     <LinkContainer to="/help"> 
-                        <Nav.Link> Help </Nav.Link>
+                        <Nav.Link className = "nav-titles-help"> Help </Nav.Link>
                     </LinkContainer>
                 </Nav>
             </Navbar.Collapse>
